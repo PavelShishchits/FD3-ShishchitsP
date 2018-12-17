8176c2ae385c5516c91d83c6cf88c04a5be7b5e8
Shishic <pasukmc@gmail.com>
2018-12-17 20:37:20 +0300

    finish ishop2 task

--- ishop2/components/ProductItem/app.js
+++ ishop2/components/ProductItem/app.js
@@ -2,28 +2,42 @@ var ProductItem = React.createClass({
 
   displayName: 'ProductItem',
 
+  getDefaultProps: function() {
+    return {deleteBtn: 'Удалить'}
+  },
+
   propTypes: {
+    id: React.PropTypes.string.isRequired,
     name: React.PropTypes.string.isRequired,
     price: React.PropTypes.number.isRequired,
     picUrl: React.PropTypes.string.isRequired,
-    balance: React.PropTypes.number.isRequired
+    balance: React.PropTypes.number.isRequired,
+    selectedProduct: React.PropTypes.string,
+    highlightProduct: React.PropTypes.func,
+    deleteProduct: React.PropTypes.func
   },
 
-  highlightRow: function(e) {
-    const curTrg = e.currentTarget;
-    curTrg.classList.add('highlight');
-    this.props.cbSelected(curTrg);
+  deleteProduct: function(e) {
+     e.stopPropagation();
+     this.props.cbDeleted(this.props);
+  },
+
+  highlightProduct: function() {
+    this.props.cbSelected(this.props.id);
   },
 
   render: function () {
 
-    return React.DOM.tr({className: 'product__item', onClick: this.highlightRow},
+    return React.DOM.tr({className: `product__item ${this.props.id === this.props.selectedProduct ? `highlight` : ``}`, onClick: this.highlightProduct},
       React.DOM.td({className: 'product__pic'},
         React.DOM.img({src: `img/${this.props.picUrl}`, alt: this.props.name})
       ),
       React.DOM.td({className: 'product__name'}, this.props.name),
       React.DOM.td({className: 'product__price'}, this.props.price),
-      React.DOM.td({className: 'product__balance'}, this.props.balance)
+      React.DOM.td({className: 'product__balance'}, this.props.balance),
+      React.DOM.td({className: 'product__delete'},
+        React.DOM.button({className: 'btn delete-btn', onClick: this.deleteProduct}, this.props.deleteBtn)
+      )
     )
   }
 });
--- ishop2/components/ProductList/app.js
+++ ishop2/components/ProductList/app.js
@@ -3,7 +3,10 @@ var ProductList = React.createClass({
   displayName: 'ProductList',
 
   getDefaultProps: function() {
-    return { shopName: 'iShop' }
+    return {
+      shopName: 'iShop',
+      emptyFallbackPhrase: 'Товаров больше не осталось'
+    }
   },
 
   getInitialState: function() {
@@ -13,8 +16,26 @@ var ProductList = React.createClass({
     }
   },
 
-  onProductClick: function(product) {
-    this.setState({selectedProduct: product})
+  userConfirmation: function(prodToDelete) {
+    return confirm(`Вы действительно хотите удалить ${prodToDelete.name}`);
+  },
+
+  onProductDelete: function(prodToDelete) {
+    if (!this.userConfirmation(prodToDelete)) {
+      return false;
+    }
+    const renewedList = this.state.productList.reduce((totalList, product) => {
+      if (prodToDelete.id !== product.id) {
+        totalList.push(product);
+      }
+      return totalList;
+    }, []);
+
+    this.setState({productList: renewedList});
+  },
+
+  onProductClick: function(productId) {
+    this.setState({selectedProduct: productId})
   },
 
   propTypes: {
@@ -25,27 +46,35 @@ var ProductList = React.createClass({
         name: React.PropTypes.string.isRequired,
         price: React.PropTypes.number.isRequired,
         picUrl: React.PropTypes.string.isRequired,
-        balance: React.PropTypes.number.isRequired
+        balance: React.PropTypes.number.isRequired,
+        selectedProduct: React.PropTypes.string
       })
     )
   },
   
   render: function () {
-    console.log(this.state);
 
     return React.DOM.div({className: 'product'},
       React.DOM.h1({className: 'shopName'}, this.props.shopName),
       React.DOM.table({className: 'product__table'},
         React.DOM.tbody(null,
+          this.state.productList.length ?
           this.state.productList.map((prod) => {
-            return React.createElement(ProductItem, {key: prod.id,
+            return React.createElement(ProductItem, {
+              key: prod.id,
+              id: prod.id,
               name: prod.name,
               price: prod.price,
               picUrl: prod.picUrl,
               balance: prod.balance,
-              cbSelected: this.onProductClick
+              cbSelected: this.onProductClick,
+              selectedProduct: this.state.selectedProduct,
+              cbDeleted: this.onProductDelete
             })
           })
+          : React.DOM.tr({className: 'product__item product__item--empty'},
+            React.DOM.td({}, this.props.emptyFallbackPhrase)
+            )
         )
       )
     )
