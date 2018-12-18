a1b9a1425789fd3ed9860b0b07f7ac938c47c5f6
Shishic <pasukmc@gmail.com>
2018-12-18 22:52:23 +0300

    finish filter task

--- filter/components/Filter/app.js
+++ filter/components/Filter/app.js
@@ -2,21 +2,18 @@ const Filter = React.createClass({
 
     'displayName': 'Filter',
 
-    getDefaultProps: function() {
-        return {fallbackText: 'Совпадений нет'}
-    },
-
     getInitialState: function() {
         return {
             wordsList: this.props.words,
+            filterText: '',
             isChecked: false
         }
     },
 
     propTypes: {
-        wordsList: React.PropTypes.arrayOf(
+        words: React.PropTypes.arrayOf(
             React.PropTypes.string.isRequired
-        )
+        ).isRequired
     },
 
     checkBoxHandler: function() {
@@ -25,36 +22,33 @@ const Filter = React.createClass({
 
     sortList: function() {
         if (this.state.isChecked) {
-            this.setState({wordsList: this.state.wordsList.slice().sort()});
+            this.setState({wordsList: this.props.words.slice().sort()});
         } else {
             this.setState({wordsList: this.props.words});
         }
     },
 
-    filterList: function(e) {
-        const trgValue = e.target.value;
-        const regExp = new RegExp(trgValue, 'gi');
-        const filteredWords = this.props.words.filter((word) => {
-            return word.search(regExp) >= 0;
-        });
-        trgValue.length > 0 ? this.setState((currState, props) => {return {wordsList: filteredWords}}) : this.setState((currState, props) => {return {wordsList: this.props.words}});
+    inputHandler: function(e) {
+        this.setState({filterText: e.target.value});
     },
 
     render: function () {
-        console.log(this.state.wordsList);
+        console.log(this.state.wordsList.length);
         return React.DOM.div({className: 'filter'},
             React.DOM.div({className: 'filter__input-wrap'},
                 React.DOM.div({className: 'filter__check-wrap'},
                     React.DOM.input({type: 'checkbox', name: 'sort', checked: this.state.isChecked, onClick: this.checkBoxHandler}),
                 ),
-                React.DOM.input({type: 'text', name: 'filter', onChange: this.filterList})
+                React.DOM.input({type: 'text', name: 'filter', onChange: this.inputHandler})
             ),
             React.DOM.ul({className: 'filter__list'},
-                this.state.wordsList.length ?
-                this.state.wordsList.map((word) => {
-                    return React.DOM.li({className: 'filter__item', key: word}, word)
-                })
-                : React.DOM.li({className: 'filter__item'}, this.props.fallbackText)
+                this.state.wordsList
+                    .filter((word) => {
+                        return word.toLowerCase().indexOf(this.state.filterText.toLowerCase()) >= 0;
+                    })
+                    .map((word) => {
+                        return React.DOM.li({className: 'filter__item', key: word}, word)
+                    })
             )
         );
     }
