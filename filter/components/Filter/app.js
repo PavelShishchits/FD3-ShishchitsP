dc8b52efcd2e5e7edf439577e3971ff4672b470b
Shishic Pavel <shishic@db.by>
2018-12-18 14:56:59 +0300

    add sort function on checkbox click

--- filter/components/Filter/app.js
+++ filter/components/Filter/app.js
@@ -2,23 +2,36 @@ const Filter = React.createClass({
 
     'displayName': 'Filter',
 
+    propTypes: {
+        wordsList: React.PropTypes.arrayOf(
+            React.PropTypes.string.isRequired
+        )
+    },
+
     getInitialState: function() {
       return {
-        wordsList: this.props.words
+        wordsList: this.props.words,
+        sortOn: false
       }
     },
 
     sortList: function(e) {
-        console.log(e.target.value);
+        this.state.sortOn = !this.state.sortOn;
+        if (this.state.sortOn) {
+            this.setState({wordsList: this.state.wordsList.slice().sort()});
+        } else {
+            this.setState({wordsList: this.props.words});
+        }
+
     },
 
     render: function () {
         return React.DOM.div({className: 'filter'},
             React.DOM.div({className: 'filter__input-wrap'},
                 React.DOM.div({className: 'filter__check-wrap'},
-                    React.DOM.input({type: 'checkbox', onClick: this.sortList}),
+                    React.DOM.input({type: 'checkbox', name: 'sort', defaultChecked: false, onClick: this.sortList}),
                 ),
-                React.DOM.input({type: 'text'})
+                React.DOM.input({type: 'text', name: 'filter'})
             ),
             React.DOM.ul({className: 'filter__list'},
                 this.state.wordsList.map((word, i) => {
