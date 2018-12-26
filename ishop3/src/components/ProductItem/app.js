e8bcb15cd35611be1a5969e75bc8114042628ffe
Shishic Pavel <shishic@db.by>
2018-12-26 17:59:16 +0300

    jsx in progress

--- ishop3/src/components/ProductItem/app.js
+++ ishop3/src/components/ProductItem/app.js
@@ -31,17 +31,19 @@ class ProductItem extends React.Component {
 
   render() {
 
-    return DOM.tr({className: `product__item ${this.props.id === this.props.selectedProduct ? `highlight` : ``}`, onClick: this.highlightProduct},
-        DOM.td({className: 'product__pic'},
-            DOM.img({src: `img/${this.props.picUrl}`, alt: this.props.name})
-        ),
-        DOM.td({className: 'product__name'}, this.props.name),
-        DOM.td({className: 'product__price'}, this.props.price),
-        DOM.td({className: 'product__balance'}, this.props.balance),
-        DOM.td({className: 'product__delete'},
-            DOM.button({className: 'btn delete-btn', onClick: this.deleteProduct}, this.props.deleteBtn)
-        )
-    )
+    return (
+      <tr className={`product__item ${this.props.id === this.props.selectedProduct ? `highlight` : ``}`} onClick={this.highlightProduct}>
+        <td className='product__pic'>
+            <img src={this.props.picUrl} alt={this.props.name}/>
+        </td>
+        <td className='product__name'>{this.props.name}</td>
+        <td className='product__price'>{this.props.price}</td>
+        <td className='product__balance'>{this.props.balance}</td>
+        <td className='product__delete'>
+            <button className='btn delete-btn' onClick={this.deleteProduct}>{this.props.deleteBtn}</button>
+        </td>
+      </tr>
+    );
   }
 }
 
