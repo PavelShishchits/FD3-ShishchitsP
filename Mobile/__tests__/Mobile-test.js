ffa44334e0619f81fa650e582538caefb96becdd
Shishic Pavel <shishic@db.by>
2019-01-22 17:47:55 +0300

    Started to write tests.

---
+++ Mobile/__tests__/Mobile-test.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Mobile from '../src/components/Mobile/index';
+const mobileData = require('../src/data.json');
+
+console.log(Mobile);
+
+test('работа TestComponent', () => {
+
+  const component = renderer.create(
+    <Mobile currCompanyName={mobileData.currCompanyName} clients={mobileData.clients}/>
+  );
+
+  let componentTree = component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+    
+});
